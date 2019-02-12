0b3594e1d7a71d866acda697c101887ac95227d5
Connor Young <connorbubgames@gmail.com>
2019-02-11 20:10:21 -0500

    First edition of comp bot 2019 code

---
+++ src/main/java/org/usfirst/frc3707/Creedence/PIDInts/Constants.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc3707.Creedence.PIDInts;
+
+public class Constants
+{
+    public static int CargoSucc = 6;
+    public static int Grabber = 7;
+    public static int Winch = 8;
+    public static int Elevator = 9;
+    public static int LCargoSpit = 10;
+    public static int RCargoSpit = 11;
+    public static int BLSwerve = 12;
+    public static int BLDrive = 13;
+    public static int BrSwerve = 14;
+    public static int BRDrive = 15;
+    public static int FRSwerve = 16;
+    public static int FRDrive = 17;
+    public static int FLSwerve = 18;
+    public static int FLDrive = 19;
+    public static int FREncoder = 1;
+    public static int FLEncoder = 0;
+    public static int BLEncoder = 3;
+    public static int BREncoder = 2;
+
+}
---
+++ src/main/java/org/usfirst/frc3707/Creedence/swerve/SwerveDrive.java
@@ -0,0 +1,171 @@
+package org.usfirst.frc3707.Creedence.swerve;
+
+import org.usfirst.frc3707.Creedence.Robot;
+
+import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SwerveDrive implements PIDOutput {
+
+    private SwerveWheel rightFrontWheel;
+    private SwerveWheel leftFrontWheel;
+    private SwerveWheel leftBackWheel;
+    private SwerveWheel rightBackWheel;
+    
+    private GyroBase gyro;
+
+    private double wheelbase = 22.5;
+    private double trackwidth = 24.5;
+
+    public SwerveDrive(SwerveWheel rightFront, SwerveWheel leftFront, SwerveWheel leftBack, SwerveWheel rightBack, GyroBase gyro){
+    	this.rightFrontWheel = rightFront;
+    	this.leftFrontWheel = leftFront;
+    	this.leftBackWheel = leftBack;
+        this.rightBackWheel = rightBack;
+        
+        System.out.println("SwerveDrive Initialized");
+	    
+        this.gyro = gyro;
+    }
+    
+    public void drive(double directionX, double directionY, double rotation, boolean useGyro, boolean slowSpeed) {
+    	
+    	SmartDashboard.putNumber("directionX", directionX);
+    	SmartDashboard.putNumber("directionY", directionY);
+        SmartDashboard.putNumber("rotation", rotation);
+        // System.out.println(rotation);
+        // System.out.println(directionX);
+        // System.out.println(directionY);
+
+    	
+    	//if BOTH joystick in the center
+    	if((directionX < 0.2 && directionX > -0.2) && (directionY < 0.2 && directionY > -0.2) && (rotation < 0.2 && rotation > -0.2)) {
+    		this.rightFrontWheel.updateSpeed(0);
+        	this.leftFrontWheel.updateSpeed(0);
+        	this.leftBackWheel.updateSpeed(0);
+        	this.rightBackWheel.updateSpeed(0);
+        	return;
+    	}
+    	//if ROTATION joystick only near the center (this fixes the rotation drift)
+    	else if (rotation < 0.2 && rotation > -0.2) {
+    		rotation = 0;
+    	}
+
+    	
+    	double L = this.wheelbase; 					//distance between front and back wheels
+    	double W = this.trackwidth; 				//distance between front wheels
+        double diameter = Math.sqrt ((L * L) + (W * W)); 	//radius of circle (actually it may be the diameter?)
+        
+        directionY *= -1; 							//invert Y
+        directionX *= -1; 							//invert X
+        rotation *= -1;
+
+        double a = directionX - rotation * (L / diameter); //rear axle
+        double b = directionX + rotation * (L / diameter); //front axle
+        double c = directionY - rotation * (W / diameter); //left track
+        double d = directionY + rotation * (W / diameter); //right track
+        
+        SmartDashboard.putNumber("a", a);
+        SmartDashboard.putNumber("b", b);
+        SmartDashboard.putNumber("c", c);
+        SmartDashboard.putNumber("d", d);
+        
+        
+        // System.out.println("A, B, C, and D");
+        // System.out.println("A: " + a);
+        // System.out.println("B: " + b);
+        // System.out.println("C: "+c);
+        // System.out.println("D: " + d);
+
+
+
+        /*
+         *                FRONT
+         * 
+         *            c          d
+         *            | 		 |
+         *       b ------------------ b
+         *            |          |
+         *            |          |
+         * LEFT       |          |      RIGHT
+         *            |          |
+         *            |          |
+         *       a ------------------ a
+         *            |          |
+         *            c          d
+         * 
+         *                BACK
+         */
+
+        //set motor speeds for each wheel
+        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+        
+        //set wheel angle for each wheel
+        double backRightAngle = (Math.atan2 (a, d) / Math.PI) * 180;
+        double backLeftAngle = (Math.atan2 (a, c) / Math.PI) * 180;
+        double frontRightAngle = (Math.atan2 (b, d) / Math.PI) * 180;
+        double frontLeftAngle = (Math.atan2 (b, c) / Math.PI) * 180;
+        
+        
+
+        //System.out.println("backLeftAngle " + backLeftAngle);
+        // System.out.println("backRightAngle " + backRightAngle);
+        // System.out.println("frontRightAngle " + frontRightAngle);
+        // System.out.println("FrontLeftAngle " + frontLeftAngle );
+
+        
+        // System.out.println("Back Right Speed: " + backRightSpeed);
+        // System.out.println("Back Left Speed: " + backLeftSpeed);
+        // System.out.println("Front Right Speed: " + frontRightSpeed);
+        // System.out.println("Front Left Speed: " + frontLeftSpeed);
+        
+        // System.out.println("Back Right Angle: " + backRightAngle);
+        // System.out.println("Back Left Angle: " + backLeftAngle);
+        // System.out.println("Front Right Angle: " + frontRightAngle);
+        // System.out.println("Front Left Angle: " + frontLeftAngle);
+        
+        if(useGyro) {
+            double gyroAngle = normalizeGyroAngle(gyro.getAngle()); 
+            backRightAngle += gyroAngle;
+            backLeftAngle += gyroAngle;
+            frontRightAngle += gyroAngle;
+            frontLeftAngle += gyroAngle;
+        }
+        if(slowSpeed) {
+        	backRightSpeed *= 0.5;
+        	backLeftSpeed *= 0.5;
+        	frontRightSpeed *= 0.5;
+        	frontLeftSpeed *= 0.5;
+        }
+        
+        //update the actual motors
+    	this.rightFrontWheel.drive(frontRightSpeed, frontRightAngle);
+    	this.leftFrontWheel.drive(frontLeftSpeed, frontLeftAngle);
+    	this.leftBackWheel.drive(backLeftSpeed, backLeftAngle);
+    	this.rightBackWheel.drive(backRightSpeed, backRightAngle);
+        
+    }
+
+	@Override
+	public void pidWrite(double output) {
+		System.out.println("X");
+		System.out.println(output);
+        drive(output, 0, 0, false, false);
+	}
+
+    public double normalizeGyroAngle(double angle){
+        return (angle - (Math.floor( angle / 360) * 360) );
+    }
+
+    public void driveSimple(double speed, double angle) {
+        this.rightFrontWheel.drive(speed, angle);
+		this.leftFrontWheel.drive(speed, angle);
+		this.leftBackWheel.drive(speed, angle);
+        this.rightBackWheel.drive(speed, angle);
+    }
+
+}
