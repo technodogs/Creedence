47e659cbc500ff6f125b187b9fff13fcc3dea6fe
Nathan <nhogge@gmail.com>
2019-04-01 15:57:44 -0400

    Tongue pnuematics

--- src/main/java/org/usfirst/frc3707/Creedence/subsystems/CargoIntakeSubsystem.java
+++ src/main/java/org/usfirst/frc3707/Creedence/subsystems/CargoIntakeSubsystem.java
@@ -10,10 +10,12 @@
 
 package org.usfirst.frc3707.Creedence.subsystems;
 
-
 import org.usfirst.frc3707.Creedence.Robot;
 import org.usfirst.frc3707.Creedence.Configuration.Constants;
 import org.usfirst.frc3707.Creedence.commands.cargo.CargoIntakeCommand;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -23,6 +25,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CargoIntakeSubsystem extends Subsystem {
 
     private PWMVictorSPX cargoSucc;
+    private DoubleSolenoid tonguePneumatic = new DoubleSolenoid(Constants.PneumaticSystem.getPcmCanId(), Constants.CargoIntakeSystem.getTongueSolenoidForward(), Constants.CargoIntakeSystem.getTongueSolenoidReverse());
 
     public CargoIntakeSubsystem() {
         cargoSucc = new PWMVictorSPX(Constants.CargoIntakeSystem.getMotorControllerPWMChannel());
@@ -35,6 +38,13 @@ public class CargoIntakeSubsystem extends Subsystem {
     public void initDefaultCommand() {
 
         setDefaultCommand(new CargoIntakeCommand());
+    }
+
+    public void moveTongueOut() {
+        tonguePneumatic.set(Value.kForward);
+    }
+    public void moveTongueIn() {
+        tonguePneumatic.set(Value.kReverse);
     }
 
     public void collectCargo(double value) {
